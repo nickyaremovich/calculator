74c8dec0a830fad231a458b0f5a640029e1bb3d7
Nick Yaremovich <nickyaremovich@gmail.com>
2025-08-22 14:53:40 -0600

    implement basic arithmetic functions: add, subtract, multiply, and divide

---
+++ app.js
@@ -0,0 +1,24 @@
+function add (a, b) {
+    return a + b
+    
+};
+
+
+console.log(add(5, 2));
+
+function subtract (a, b) {
+    return a - b;
+}
+
+console.log(subtract(5, 2));
+
+function multiply (a, b) {
+    return a * b;
+}
+
+console.log(subtract(5, 2));
+
+function divide (a, b) {
+    return a / b;
+}
+console.log(divide(5, 2));
