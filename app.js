8ae1d555bb4c7b19d6b1ab6d31fc9c5a21bde561
Nick Yaremovich <nickyaremovich@gmail.com>
2025-08-22 14:55:32 -0600

    refactor: remove console log statements from arithmetic functions

--- app.js
+++ app.js
@@ -3,22 +3,16 @@ function add (a, b) {
     
 };
 
-
-console.log(add(5, 2));
-
 function subtract (a, b) {
     return a - b;
 }
 
-console.log(subtract(5, 2));
 
 function multiply (a, b) {
     return a * b;
 }
 
-console.log(multiply(5, 2));
 
 function divide (a, b) {
     return a / b;
 }
-console.log(divide(5, 2));
