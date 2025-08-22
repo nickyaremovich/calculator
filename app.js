a336b5157824a5162f57f0eda58747a985847b14
Nick Yaremovich <nickyaremovich@gmail.com>
2025-08-22 15:03:43 -0600

    add operate function via a switch

--- app.js
+++ app.js
@@ -20,3 +20,15 @@ function divide (a, b) {
 const operator = ['+', '-', '*', '/'];
 let firstOperand = 0;
 let secondOperand = 0;
+
+function operate (operator, firstOperand, secondOperand) {
+    if (!operators.includes(operator)) return 'Invalid operator';
+
+  switch (operator) {
+    case '+': return operand1 + operand2;
+    case '-': return operand1 - operand2;
+    case '*': return operand1 * operand2;
+    case '/': return operand1 / operand2;
+    default: return 'Error';
+  }
+};
